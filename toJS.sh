e0e07cd94a9bd4cc82366473ceb0a3730520be40
William Bell <william.bell@frog.za.net>
2014-06-01 21:36:42 +0200

    Added a bash script to do initial Python to JavaScript conversion.
    just does a find and replace using sed

---
+++ toJS.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# usage ./toJS source.py output.js
+sed '
+s/\t/    /g
+s/elif/else if (/
+s/else:/else {/g
+s/math./Math./g
+s/True/true/g
+s/False/false/g
+s/self/this/g
+s/def /function /g
+s/^#/\/\/g
+s/# /\/\/ /g
+s/):/) {/g
+s/str(/String(/g
+s/(this, /(/g
+s/(this)/()/g
+/global /d
+/import /d
+s/class /function /g
+s/:$/) {/g
+s/return false/return false;/
+s/return true/return true;/
+s/Math.pi/Math.PI/g
+s/Math.e/Math.E/g
+s/type/typeof/g
+s/None/null/g'  <$1 >$2
